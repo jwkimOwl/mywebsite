52aca5dce845f1ca16ae81407a556fcf227263ef
Hannah <hannah.jaywon.kim@gmail.com>
2025-01-13 10:55:27 -0500

    Created main page

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,26 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="menu-bar">
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+	    <h1 id ="Heading">
+	      Hannah J. Kim
+	    </h1>
+	    <p id ="Paragraph">
+	      This is a paragraph. You can type stuff you want to see here.
+	    </p>
+	    <p id = "Next">
+	      This is used for another paragraph.
+	    </p>
     </>
   )
 }
